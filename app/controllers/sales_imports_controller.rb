9ccef5a4a7308ac126005c6cc14ccfabeaecbb84
Gillys Dantas <gillys.ayres@gmail.com>
2021-06-09 20:06:39 -0300

    add import and show functionality

--- app/controllers/sales_imports_controller.rb
+++ app/controllers/sales_imports_controller.rb
@@ -1,2 +1,10 @@
 class SalesImportsController < ApplicationController
+  def index
+    @sales = SalesImport.all
+  end
+
+  def import
+    count = SalesImport.import params[:file]
+    redirect_to sales_imports_path, notice: "#{count} new sales entries"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sales_imports do
+    collection do
+      post :import
+    end
+  end
+
+  # POST /sales_imports/import
 end
